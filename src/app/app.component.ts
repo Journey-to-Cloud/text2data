287fffb198bb87aa509671a5b44dc35c70ed8b3a
Hari Krishna <harikrishna.yenigandla@quadrantresource.com>
2023-04-17 11:43:35 +0530

    upload buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ModelapiService } from './modelapi.service';
 import Handsontable from 'handsontable';
@@ -13,24 +13,24 @@ import { timeout } from 'rxjs';
 export class AppComponent implements OnInit {
   prompt: any;
   data: any;
-  loading:boolean = false;
-  promptText:string= '';
+  loading: boolean = false;
+  promptText: string = '';
   isMemoryOn: boolean = false;
-  fileUploaded:boolean = false;
-  successMsg:any
-  ngOnInit(): void {}
+  fileUploaded: boolean = false;
+  successMsg: any
+  ngOnInit(): void { }
   @ViewChild('fileInput') fileInput: any;
-  constructor(private modelapiService: ModelapiService,private render: Renderer2) {}
+  constructor(private modelapiService: ModelapiService, private render: Renderer2) { }
 
   query(value: any) {
     return new Promise((resolve, reject) => {
       // this.modelapiService.query(value + ' dont use limit in sql server only query').subscribe({
       this.modelapiService.query(value).subscribe({
-        next: (response:any) => {
+        next: (response: any) => {
           console.log(response);
           resolve(response);
         },
-        error: (error:any) => {
+        error: (error: any) => {
           reject(error);
         },
       });
@@ -44,21 +44,21 @@ export class AppComponent implements OnInit {
     this.data = null
     console.log('Prompt', this.prompt);
     if (!this.prompt) {
-      this.loading  = false
+      this.loading = false
       return alert('Enter Valid Text!');
     }
     await this.query(this.prompt).then((responce) => {
       this.data = responce;
-      this.loading  = false;
+      this.loading = false;
       this.prompt = '';
     });
-    if(this.data==null){
+    if (this.data == null) {
       alert("Something went wrong")
     }
     if (container) {
       new Handsontable(container, {
         data: this.data.body,
-        
+
         //colHeaders: true,
         //rowHeaders: true,
         //height: 'auto',
@@ -68,7 +68,7 @@ export class AppComponent implements OnInit {
   }
   onFileSelect() {
     let file = this.fileInput.nativeElement.files[0];
-    console.log("file uplaod: ",file)
+    console.log("file uplaod: ", file)
     //const file = event.target.files[0];
     if (!file) {
       alert("Please select a file to upload.");
@@ -76,8 +76,8 @@ export class AppComponent implements OnInit {
     }
     if (file) {
       const formData = new FormData();
-      formData.append("Data",file)
-      this.modelapiService.uploadFile(formData).subscribe((res:any)=>{
+      formData.append("Data", file)
+      this.modelapiService.uploadFile(formData).subscribe((res: any) => {
         this.successMsg = res.body
         this.fileUploaded = true
         console.log("file upload response", res);
